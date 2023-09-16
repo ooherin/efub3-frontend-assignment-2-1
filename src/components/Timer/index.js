6295cb5c095ab939f686ccbd8e2c912f2cafe5d3
ooherin <555ohr@naver.com>
2023-09-16 20:58:50 +0900

    feat: 일부 코드 수정

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -21,10 +21,7 @@ const Counter = () => {
   //렌더링 할 text를 저장해주는 state
   const [timerPlay, setTimerPlay] = useState(true);
 
-  //spendTime은 setInterval에서 반환된 ID를 저장하고, clearInterval에서
-  //이 ID값을 참조해 타이머를 멈추고 시작할 수 있다.
   const spendTime = useRef(null);
-  //132번쨰 줄 (dom접근)
   const textInput = useRef();
 
   //onClickFocusMove : input으로 focus를 해주는 함수
@@ -37,8 +34,6 @@ const Counter = () => {
 
   const dispatch = useDispatch();
   //onCLickListSave : 최종적으로 렌더링될 타임 list에 기존의 시간과, input에 적어준 text값을 저장한다.
-  //time이라는 key값으로 시간 : 분 : 초 순서대로 넣어준다.
-  //timer가 동작중이라면 타이머를 멈춰준다
   const onCLickListSave = () => {
     //timeList에 저장하기
     const newTimeList = {
@@ -68,16 +63,12 @@ const Counter = () => {
   };
 
   //onClickResetCounter: 모든 값을 초기화시켜주는 함수
-  //counter를 0으로 바꿔준다
-  //만약 play상태에서 눌러지면 play를 false로 바꿔준다음 실행시킨다.
   const onClickResetCounter = () => {
     setCount(0);
     clearInterval(spendTime.current);
     setTimerPlay(true);
   };
 
-  //count가 변할때마다 시간, 분, 초를 계산해서 setTime에 넣어주는 함수를 실행한다.
-  //10미만이면 앞에 0을 붙여 형식을 맞춰준다.
   useEffect(() => {
     let hour = Math.floor(count / 60) % 60;
     let minute = Math.floor(count / 3600);
